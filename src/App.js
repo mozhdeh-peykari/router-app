a0f33ab73eb368d0081459701289350f7e90cf83
Mozhdeh Peykari <mozhdeh.paykari@gmail.com>
2022-05-19 13:42:40 +0430

    Pass route parameters

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ class App extends Component {
       <div>
         <NavBar />
         <Switch>
-          <Route path="/products" component={Products}/>
-          <Route path="/posts" component={Posts}/>
+          <Route path="/products/:id" component={ProductDetails}/>
+          <Route path="/products" render = {(props) => <Products sortBy="newest" {...props}/>}/>
+          <Route path="/posts/:year?/:month?" component={Posts}/>
           <Route path="/admin" component={Dashboard}/>
           <Route path="/" component={Home}/>
         </Switch>
